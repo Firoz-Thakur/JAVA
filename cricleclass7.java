bbedf265bb94f2cd3269c30f819c86fe3b4930fc
Firoz kumar <56572016+firozbaba@users.noreply.github.com>
2020-02-12 19:05:29 +0530

    Create cricleclass7.java

---
+++ cricleclass7.java
@@ -0,0 +1,37 @@
+class Circle
+{ 
+	 private double radius;
+	 
+	 void setRad(double r)
+	 {
+		 radius=r;
+		 
+	 }
+	 double getRad()
+	 {
+		return radius;
+	 }
+	 void display()
+	 {
+		System.out.println(3.14 *radius * radius);
+	        System.out.println(2 *3.14*radius);
+       } 
+}
+public class demo2
+{
+
+	public static void main(String[] args)
+{
+		// TODO Auto-generated method stub
+	   Circle ar=new Circle();
+	   Circle ar1=new Circle();
+	   ar.setRad(5.3);
+	   ar1.setRad(5.4);
+	   System.out.println(ar.getRad());
+	   System.out.println(ar1.getRad());
+           ar.display();
+           ar1.display();
+   
+}
+
+}
